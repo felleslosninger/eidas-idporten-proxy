8e809dbd3458a78fa244b498a5474c6c15bbbd05
Anne Marte Hjemås <kons-ahj@digdir.no>
2024-05-03 13:04:10 +0200

    får tak i melding i cache når navnene er riktige (#5)

--- src/main/java/no/idporten/eidas/proxy/integration/specificcommunication/config/EidasCacheProperties.java
+++ src/main/java/no/idporten/eidas/proxy/integration/specificcommunication/config/EidasCacheProperties.java
@@ -1,6 +1,5 @@
 package no.idporten.eidas.proxy.integration.specificcommunication.config;
 
-import jakarta.validation.constraints.NotEmpty;
 import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.validation.annotation.Validated;
@@ -11,29 +10,27 @@ import org.springframework.validation.annotation.Validated;
 @Validated
 public class EidasCacheProperties {
 
-    public static final String LIGHT_REQUEST = "light-request";
-    public static final String LIGHT_RESPONSE = "light-response";
+    public static final String LIGHT_REQUEST = "nodeSpecificProxyserviceRequestCache";
+    public static final String LIGHT_RESPONSE = "specificNodeProxyserviceResponseCache";
     public static final String IDP_REQUEST = "correlation-map";
-    private static final String PREFIX_TEMPLATE = "%s:%s:%s";
+    private static final String PREFIX_TEMPLATE = "%s:%s";
 
-    @NotEmpty
-    private String name = "eidas-proxy";
 
-    private String requestSecret = "secret";
-    private String responseSecret = "secret";
+    private String requestSecret;
+    private String responseSecret;
     private String algorithm = "SHA256";
     private String responseIssuerName = "specificCommunicationDefinitionProxyserviceRequest";
     private String requestIssuerName = "specificCommunicationDefinitionProxyserviceResponse";
     public String getLightRequestPrefix(String id) {
-        return PREFIX_TEMPLATE.formatted(name, LIGHT_REQUEST, id);
+        return PREFIX_TEMPLATE.formatted(LIGHT_REQUEST, id);
     }
 
     public String getLightResponsePrefix(String id) {
-        return PREFIX_TEMPLATE.formatted(name, LIGHT_RESPONSE, id);
+        return PREFIX_TEMPLATE.formatted(LIGHT_RESPONSE, id);
     }
 
     public String getIdpRequestPrefix(String id) {
-        return PREFIX_TEMPLATE.formatted(name, IDP_REQUEST, id);
+        return PREFIX_TEMPLATE.formatted(IDP_REQUEST, id);
     }
 
 }
