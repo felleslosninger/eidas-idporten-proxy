0731354b4d9f5aab902ac89add49d19c5356f8c7
Anne Marte Hjemås <kons-ahj@digdir.no>
2024-07-03 10:39:48 +0200

    fjerne deprecated jwks-klasser

--- src/main/java/no/idporten/eidas/proxy/integration/idp/config/OIDCIntegrationConfiguration.java
+++ src/main/java/no/idporten/eidas/proxy/integration/idp/config/OIDCIntegrationConfiguration.java
@@ -1,23 +1,22 @@
 package no.idporten.eidas.proxy.integration.idp.config;
 
-import com.nimbusds.jose.jwk.source.DefaultJWKSetCache;
-import com.nimbusds.jose.jwk.source.RemoteJWKSet;
-import com.nimbusds.jose.proc.JWKSecurityContext;
+import com.nimbusds.jose.jwk.source.JWKSource;
+import com.nimbusds.jose.jwk.source.JWKSourceBuilder;
 import com.nimbusds.jose.proc.JWSKeySelector;
 import com.nimbusds.jose.proc.JWSVerificationKeySelector;
-import com.nimbusds.jose.util.ResourceRetriever;
+import com.nimbusds.jose.proc.SecurityContext;
 import com.nimbusds.oauth2.sdk.id.ClientID;
 import com.nimbusds.oauth2.sdk.id.Issuer;
 import com.nimbusds.openid.connect.sdk.op.OIDCProviderMetadata;
 import com.nimbusds.openid.connect.sdk.validators.IDTokenValidator;
 import lombok.extern.slf4j.Slf4j;
-import no.idporten.eidas.proxy.integration.idp.LoggingResourceRetriever;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.net.MalformedURLException;
+import java.time.Duration;
 import java.util.HashSet;
-import java.util.concurrent.TimeUnit;
 
 @Slf4j
 @Configuration
@@ -36,20 +35,18 @@ public class OIDCIntegrationConfiguration {
     }
 
     @Bean
-    public RemoteJWKSet<JWKSecurityContext> remoteJWKSet(OIDCIntegrationProperties properties, OIDCProviderMetadata oidcProviderMetadata) throws Exception {
-        ResourceRetriever resourceRetriever = new LoggingResourceRetriever(
-                properties.getConnectTimeOutMillis(),
-                properties.getReadTimeOutMillis());
-        DefaultJWKSetCache jwkSetCache = new DefaultJWKSetCache(
-                properties.getJwksCacheLifetimeMinutes(),
-                properties.getJwksCacheRefreshMinutes(),
-                TimeUnit.MINUTES);
-        return new RemoteJWKSet<>(oidcProviderMetadata.getJWKSetURI().toURL(), resourceRetriever, jwkSetCache);
+    public JWKSource<SecurityContext> remoteJWKSet(OIDCIntegrationProperties properties, OIDCProviderMetadata oidcProviderMetadata) throws MalformedURLException {
+        return JWKSourceBuilder.create(oidcProviderMetadata.getJWKSetURI().toURL())
+                .rateLimited(true)
+                .cache(Duration.ofMinutes(properties.getJwksCacheLifetimeMinutes()).toMillis(),
+                        Duration.ofMinutes(properties.getJwksCacheRefreshMinutes()).toMillis())
+                .build();
+
     }
 
     @Bean
-    public IDTokenValidator idTokenValidator(OIDCIntegrationProperties properties, OIDCProviderMetadata oidcProviderMetadata, RemoteJWKSet<JWKSecurityContext> remoteJWKSet) {
-        JWSKeySelector<JWKSecurityContext> keySelector = new JWSVerificationKeySelector<>(
+    public IDTokenValidator idTokenValidator(OIDCIntegrationProperties properties, OIDCProviderMetadata oidcProviderMetadata, JWKSource<SecurityContext> remoteJWKSet) {
+        JWSKeySelector<SecurityContext> keySelector = new JWSVerificationKeySelector<>(
                 new HashSet<>(oidcProviderMetadata.getIDTokenJWSAlgs()),
                 remoteJWKSet);
         return new IDTokenValidator(
